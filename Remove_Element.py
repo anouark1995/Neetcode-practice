80fd191512e99cf9382d86eb9b5da6bc4e71b8d2
anouark1995 <kacimianouar@gmail.com>
2026-04-20 16:49:53 -0400

    New problem submission to cloud

--- Remove_Element.py
+++ Remove_Element.py
@@ -1,2 +1,7 @@
-wow
-just testing staged changes
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        nums = [num for num in nums if num != val]
+        return nums
+    
+solution = Solution()
+print(solution.removeElement([1,1,2,3,4],1))
