3de778458e8e653c43c49bd9d2f284519e4c30f8
anouark1995 <kacimianouar@gmail.com>
2026-05-14 18:11:18 -0400

    first Medium

---
+++ Group_anagrams.py
@@ -0,0 +1,18 @@
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        dict = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                 count[ord(c) - ord("a")] += 1
+
+            dict[tuple(count)].append(s)
+        return dict.values()
+
+    
+sol = Solution()
+print(sol.groupAnagrams(['cat','tac','bat','atc','eat','tea','ate']))
